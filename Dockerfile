f1e7635edd59fc42a5570c8ca394840d51f559fc
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-12-22 16:49:29 +0100

    follow up

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.8-slim as builder
 
 ENV PIP_EXTRA_INDEX_URL=https://www.piwheels.org/simple
 
+COPY requirements.txt /tmp/requirements.txt
+
 RUN set -x \
     # Install buildtime packages
     && apt-get update && apt-get install -y --no-install-recommends \
@@ -14,16 +16,15 @@ RUN set -x \
 
 # build python wheels
 WORKDIR /wheels
-COPY . /tmp
 RUN pip wheel uvloop cchardet aiodns brotlipy \
-    && pip wheel -r /tmp/requirements.txt \
-    && pip wheel /tmp
+    && pip wheel -r /tmp/requirements.txt
     
 #### FINAL IMAGE
 FROM python:3.8-slim AS final-image
 
 WORKDIR /wheels
 COPY --from=builder /wheels /wheels
+COPY . /app
 RUN set -x \
     # Install runtime dependency packages
     && apt-get update \
@@ -32,13 +33,15 @@ RUN set -x \
         tzdata \
         ca-certificates \
         openssl \
-    # install emulated hue (and all it's dependencies) using the prebuilt wheels
-    && pip install --no-cache-dir -f /wheels music_assistant \
+    # install prebuilt wheels
+    && pip install --no-cache-dir -f /wheels -r /app/requirements.txt \
     # cleanup
     && rm -rf /tmp/* \
     && rm -rf /wheels \
     && rm -rf /var/lib/apt/lists/* \
     && rm -rf /root/*
+
+WORKDIR /app
 
 ENV DEBUG=false
 ENV VERBOSE=false
@@ -50,4 +53,4 @@ EXPOSE 443/tcp
 
 VOLUME [ "/data" ]
 
-ENTRYPOINT ["python3", "--config", "/data"]
+ENTRYPOINT ["python3", "-m", "emulated_hue", "--data", "/data"]
--- emulated_hue/__main__.py
+++ emulated_hue/__main__.py
@@ -21,44 +21,43 @@ if __name__ == "__main__":
     logger.addHandler(consolehandler)
     logger.setLevel(logging.INFO)
 
-    default_data_dir = (
-        os.getenv("APPDATA") if os.name == "nt" else os.path.expanduser("~")
-    )
-    default_data_dir = os.path.join(default_data_dir, ".emulated_hue")
+    if IS_SUPERVISOR:
+        default_data_dir = "/data"
+    else:
+        default_data_dir = (
+            os.getenv("APPDATA") if os.name == "nt" else os.path.expanduser("~")
+        )
+        default_data_dir = os.path.join(default_data_dir, ".emulated_hue")
 
     parser = argparse.ArgumentParser(description="Home Assistant HUE Emulation.")
-    if not IS_SUPERVISOR:
-        parser.add_argument(
-            "--data",
-            type=str,
-            help="path to store config files",
-            default=default_data_dir,
-        )
-        parser.add_argument(
-            "--url", type=str, help="url to HomeAssistant", required=True
-        )
-        parser.add_argument(
-            "--token",
-            type=str,
-            help="Long Lived Token for HomeAssistant",
-            required=True,
-        )
+
+    parser.add_argument(
+        "--data", type=str, help="path to store config files", default=default_data_dir
+    )
+    parser.add_argument(
+        "--url",
+        type=str,
+        help="url to HomeAssistant",
+        required=True,
+        default=os.getenv("HASS_URL", "http://hassio/homeassistant"),
+    )
+    parser.add_argument(
+        "--token",
+        type=str,
+        help="Long Lived Token for HomeAssistant",
+        required=True,
+        default=os.getenv("HASS_TOKEN", os.getenv("HASSIO_TOKEN")),
+    )
     parser.add_argument(
         "--verbose", type=bool, help="Enable more verbose logging", default=False
     )
 
-    # auto detect hassio
-    if IS_SUPERVISOR:
-        token = os.environ["HASSIO_TOKEN"]
-        datapath = "/data"
-        url = "http://hassio/homeassistant"
-    else:
-        args = parser.parse_args()
-        datapath = args.data
-        url = args.url
-        token = args.token
-        if args.verbose:
-            logger.setLevel(logging.DEBUG)
+    args = parser.parse_args()
+    datapath = args.data
+    url = args.url
+    token = args.token
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
     hue = HueEmulator(datapath, url, token)
     run(hue.start(), use_uvloop=True)
