4472feb5d0100e4f7254b9cc2e99bdcc420e0095
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-01 21:52:07 +0100

    do not return empty areas/rooms

--- emulated_hue/api.py
+++ emulated_hue/api.py
@@ -819,6 +819,9 @@ class HueApi:
                 result[group_id]["state"]["any_on"] = True
             if lights_on == len(result[group_id]["lights"]):
                 result[group_id]["state"]["all_on"] = True
+            # do not return empty areas/rooms
+            if len(result[group_id]["lights"]) == 0:
+                result.pop(group_id, None)
 
         return result
 
