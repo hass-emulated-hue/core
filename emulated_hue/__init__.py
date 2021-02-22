cbd964754442a805b7c18b485094f881f5764ddf
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-02-22 23:14:04 +0100

    Revert "adapt changes for updated hass client 0.1.0 (#111)"
    This reverts commit ee390791f7e476bffa69b1d8906c85ed1f47fa34.

--- emulated_hue/__init__.py
+++ emulated_hue/__init__.py
@@ -1,9 +1,8 @@
 """Support for local control of entities by emulating a Philips Hue bridge."""
 import asyncio
 import logging
-from typing import Optional
 
-from hass_client import HomeAssistantClient
+from hass_client import HomeAssistant
 
 from .api import HueApi
 from .config import Config
@@ -22,13 +21,11 @@ class HueEmulator:
         hass_token: str,
         http_port: int,
         https_port: int,
-    ) -> None:
+    ):
         """Create an instance of HueEmulator."""
         self._loop = None
         self._config = Config(self, data_path, http_port, https_port)
-        self._hass: Optional[HomeAssistantClient] = None
-        self._hass_url = hass_url
-        self._hass_token = hass_token
+        self._hass = HomeAssistant(url=hass_url, token=hass_token)
         self._api = HueApi(self)
 
     @property
@@ -37,7 +34,7 @@ class HueEmulator:
         return self._config
 
     @property
-    def hass(self) -> HomeAssistantClient:
+    def hass(self) -> HomeAssistant:
         """Return the Home Assistant instance."""
         return self._hass
 
@@ -46,11 +43,10 @@ class HueEmulator:
         """Return the running event loop."""
         return self._loop
 
-    async def async_start(self) -> None:
+    async def async_start(self):
         """Start running the Hue emulation."""
         self._loop = asyncio.get_running_loop()
-        self._hass = HomeAssistantClient(url=self._hass_url, token=self._hass_token)
-        await self._hass.connect()
+        await self._hass.async_connect()
         await self._api.async_setup()
         self.loop.create_task(async_setup_discovery(self.config))
         # remove legacy light_ids config
@@ -58,8 +54,7 @@ class HueEmulator:
             await self.config.async_delete_storage_value("light_ids")
         # TODO: periodic search for renamed/deleted entities/areas
 
-    async def async_stop(self) -> None:
+    async def async_stop(self):
         """Stop running the Hue emulation."""
         LOGGER.info("Application shutdown")
-        await self._hass.disconnect()
         await self._api.async_stop()
