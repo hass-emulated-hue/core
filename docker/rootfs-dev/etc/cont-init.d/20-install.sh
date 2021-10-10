ddf68708e53939af8e06f9f1b63af8e66100f3dd
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-10-10 11:13:00 -0500

    Unify Docker Repos (#212)
    * Merge docker repos
    
    Removes base image -> use docker caching
    Removes s6-overlay-base -> Include files here
    
    * Cleanup
    
    * Move cache back
    
    * Remove base trigger
    
    * Use inline cache
    
    * Remove inline
    
    * Rename final
    
    * Debug cache miss
    
    * Move label args
    
    * Test build

---
+++ docker/rootfs-dev/etc/cont-init.d/20-install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Emulated Hue
+# This file installs the Emulated Hue version if specified
+# ==============================================================================
+
+declare release_version
+
+if bashio::config.has_value 'tag_commit_or_branch'; then
+    release_version=$(bashio::config 'tag_commit_or_branch')
+else
+    release_version=${TAG_COMMIT_OR_BRANCH:-master}
+fi
+
+colon_count=$(tr -dc ':' <<<"$release_version" | awk '{ print length; }')
+repo_name="core"
+
+if [[ "$colon_count" == 1 ]]; then
+  IFS=':' read -r -a array <<< "$release_version"
+  username=${array[0]}
+  ref=${array[1]}
+elif [ "$colon_count" == 2 ]; then
+  IFS=':' read -r -a array <<< "$release_version"
+  username=${array[0]}
+  repo_name=${array[1]}
+  ref=${array[2]}
+else
+  username="hass-emulated-hue"
+  ref=$release_version
+fi
+full_url="https://github.com/${username}/${repo_name}/archive/${ref}.tar.gz"
+bashio::log.info "Installing Emulated Hue version '${release_version}' (${full_url})..."
+curl -Lo /tmp/emulator.tar.gz "${full_url}"
+mkdir -p /tmp/emulator
+tar zxvf /tmp/emulator.tar.gz --strip 1 -C /tmp/emulator
+mv /tmp/emulator/emulated_hue .
+bashio::log.info "Installed successfully!"
